80eb90b88b7558103cd3a4d8c631f8734a26711a
AGHUser2749 <dawoemail12@gmail.com>
2024-12-17 20:49:06 +0100

    Update 0.0.1 Alpha

---
+++ script.js
@@ -0,0 +1,7 @@
+const infobutton = document.getElementById("githubinfobutton")
+
+function opengithub() {
+    window.open("https://www.youtube.com", "_blank");
+}
+
+infobutton.addEventListener("click", opengithub())
